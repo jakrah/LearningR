f1962da752140d5d3f3a800692113ad2465c1e79
Jakaria Rahman <jakaria@gmail.com>
2023-01-17 13:40:47 +0100

    showin off mutate and filter

--- R/learning.R
+++ R/learning.R
@@ -59,3 +59,51 @@ nhanes_small %>%
     rename (
         Physically_active = phys_active
     )
+nhanes_small %>%
+    select (bp_sys_ave, education)
+nhanes_small %>%
+    rename(bp_sys = bp_sys_ave,
+           bp_dia = bp_dia_ave
+           )
+nhanes_small %>%
+select(bmi, contains("age"))
+
+nhanes_small %>%
+    select(starts_with ("bp")
+           rename (bp_systolic = bp_sys_ave))
+
+# Filtering rows ----------------------------------------------------------
+
+nhanes_small %>%
+    filter(phys_active != "No")
+
+nhanes_small %>%
+    filter(bmi >= 25,
+           phys_active == "No")
+
+nhanes_small %>%
+filter(bmi == 25 |
+           phys_active == "No")
+
+# Arranging rows ----------------------------------------------------------
+
+nhanes_small %>%
+    arrange(desc(age), bmi)
+
+# Mutating columns --------------------------------------------------------
+
+nhanes_small %>%
+    mutate(age = age * 12 )
+
+nhanes_small %>%
+    mutate(age_month = age * 12 )
+
+nhanes_update <- nhanes_small %>%
+    mutate(age_month = age * 12,
+           logged_bmi = log(bmi),
+           age_weeks = age_month * 4,
+           old = if_else(age >= 30,
+                         "old",
+                         "young"))
+
+
